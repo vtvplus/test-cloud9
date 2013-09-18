254c96689bbf45dfb948ced3a2cf89d4aabe3bea
Nha Tran <khanhnha@gmail.com>
2013-09-18 08:22:28 -0400

    test commit from c9.io

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$siteName = "VTV Plus"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome</title>
+<head>
+<body>
+    <h1>Welcome to <a href="http://vtvplus.vn"><?php echo $siteName; ?></a></h1>
+</body>
+</html>
